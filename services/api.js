be790d95f24b5ca50113e90c0a25e92bfa506060
Rajat-264 <handerajat@gmail.com>
2025-06-19 18:27:01 +0530

    Changes in domain

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://note-maker-backend-ioqg.onrender.com/api', 
+  baseURL: 'https://note-maker-backend-ecxb.onrender.com/api', 
 });
 
 API.interceptors.request.use((req) => {
